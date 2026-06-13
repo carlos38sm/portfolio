ed6ecb16227657a2a66d4d602de2c948c5831bd7
Carlos Sánchez Miranda <carlos38sm@gmail.com>
2026-06-13 19:16:19 +0200

    Eliminar telemetria de Google

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from './components/header/header';
-import { CookieConsentComponent } from './components/cookie-consent/cookie-consent';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, Header, CookieConsentComponent],
+  imports: [RouterModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
